fa09d2cba71b989b3cb3310e97f166b19abe0e8f
Jorge Cupi <jorgecupi@hotmail.com>
2022-05-17 13:56:45 -0800

    FIX. Remove 'second message' that had no really use case for the sample

--- code/src/main/java/com/jorgecupi/java/singleentitytransaction/service/SingleEntityTransactionService.java
+++ code/src/main/java/com/jorgecupi/java/singleentitytransaction/service/SingleEntityTransactionService.java
@@ -13,38 +13,35 @@ import com.azure.messaging.servicebus.models.CompleteOptions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
+  
 @Service
 public class SingleEntityTransactionService {
-
-  @Autowired
-  private ServiceBusSenderClientBuilder senderClientBuilder;
-
-  @Autowired
-  private ServiceBusReceiverClientBuilder receiverClientBuilder;
-
-  private static final String FIRST_MESSAGE_TO_SEND = "This is the first message";
-  private static final String SECOND_MESSAGE_TO_SEND = "This is the second message";
-
-  public void singleTransactionHandler() {
-      
-    var firstMessage = createServiceBusMessage(FIRST_MESSAGE_TO_SEND);
-    var secondMessage = createServiceBusMessage(SECOND_MESSAGE_TO_SEND);
-    var senderClient = senderClientBuilder.buildClient();
-    var receiverClient = receiverClientBuilder.buildClient();
-
-    senderClient.sendMessage(firstMessage);
-    var receivedMessage = receiverClient.receiveMessages(1, Duration.ofMillis(15000)).iterator().next();
-    ServiceBusTransactionContext transactionId = senderClient.createTransaction();
-    senderClient.sendMessage(secondMessage);
-    receiverClient.complete(receivedMessage, new CompleteOptions().setTransactionContext(transactionId));
-    senderClient.commitTransaction(transactionId);    
-  }
-
-  private ServiceBusMessage createServiceBusMessage(String messageToSend) {
-    var serviceBusMessage = new ServiceBusMessage(
-        BinaryData.fromBytes(messageToSend.getBytes(StandardCharsets.UTF_8)));
-    serviceBusMessage.setContentType("application/json");
-    return serviceBusMessage; 
-  }
+  
+    @Autowired
+    private ServiceBusSenderClientBuilder senderClientBuilder;
+  
+    @Autowired
+    private ServiceBusReceiverClientBuilder receiverClientBuilder;
+  
+    private static final String FIRST_MESSAGE_TO_SEND = "This is the first message";
+    
+    public void singleTransactionHandler() {
+        
+        var firstMessage = createServiceBusMessage(FIRST_MESSAGE_TO_SEND);
+        var senderClient = senderClientBuilder.buildClient();
+        var receiverClient = receiverClientBuilder.buildClient();
+    
+        senderClient.sendMessage(firstMessage);
+        var receivedMessage = receiverClient.receiveMessages(1, Duration.ofMillis(15000)).iterator().next();
+        ServiceBusTransactionContext transactionId = senderClient.createTransaction();
+        receiverClient.complete(receivedMessage, new CompleteOptions().setTransactionContext(transactionId));
+        senderClient.commitTransaction(transactionId);    
+    }
+  
+    private ServiceBusMessage createServiceBusMessage(String messageToSend) {
+        var serviceBusMessage = new ServiceBusMessage(
+            BinaryData.fromBytes(messageToSend.getBytes(StandardCharsets.UTF_8)));
+        serviceBusMessage.setContentType("application/json");
+        return serviceBusMessage; 
+    }
 }
